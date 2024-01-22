2ce27320abd0368a1681666526b34c91ce67ed05
Enes Şahin <97321156+enessahin9@users.noreply.github.com>
2024-01-22 09:51:58 +0300

    update project folder

---
+++ softito-backend-dictionary-search/softito-backend-dictionary-search/Program.cs
@@ -0,0 +1,33 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using Example.Datas;
+using Newtonsoft.Json;
+using softito_backend_dictionary_search;
+
+var personnelUsers = JsonConvert.DeserializeObject<IList<Personnel>>(PersonnelDatas.PersonnelJson);
+var studentUsers = JsonConvert.DeserializeObject<IList<Student>>(StudentDatas.StudentJson);
+var jobberUsers = JsonConvert.DeserializeObject<IList<Jobber>>(JobberDatas.JobberJson);
+
+IDictionary<string, IList<string>> indexes = new Dictionary<string, IList<string>>();
+IDictionary<string, IUser> fastList = new Dictionary<string, IUser>();
+
+fastList.AddToDictionary(personnelUsers.Select(user => (user as IUser)).ToList(), indexes);
+fastList.AddToDictionary(studentUsers.Select(user => (user as IUser)).ToList(), indexes);
+fastList.AddToDictionary(jobberUsers.Select(user => (user as IUser)).ToList(), indexes);
+
+var findAll = FindByIndex("ejeannequin4z");
+JsonConvert.SerializeObject(findAll).WriteLine();
+//Console.ReadKey(); //TODO
+
+JsonConvert.SerializeObject(indexes).WriteLine();
+Console.ReadKey();
+
+IList<IUser>? FindByIndex(string search)
+{
+	if (indexes.ContainsKey(search))
+	{
+		var findedKeys = indexes[search];
+		return findedKeys.Select(key => fastList[key]).ToList();
+	}
+	return null;
+}
---
+++ softito-backend-dictionary-search/softito-backend-dictionary-search/Student.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace softito_backend_dictionary_search;
+
+	public interface IStudent : IUser
+	{
+		string _studentNo { get; set; }
+		int _absenteeism { get; set; }
+		byte _marks { get; set; }
+
+	}
+	public class Student : User, IStudent
+	{
+		public string _studentNo { get; set; }
+		public int _absenteeism { get; set; }
+		public byte _marks { get; set; }
+		public  Student() { }
+		public Student(string userName, string password, bool isActive, string identityNo, int absenteeism, byte marks, string studentNo) : base(identityNo, userName, password, isActive )
+
+	{
+		_studentNo = studentNo;
+		_absenteeism = absenteeism;
+		_marks = marks;
+	}
+}
